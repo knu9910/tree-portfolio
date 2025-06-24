b730e667f93f74c10807adaef8d59ea10e168f6b
knu9910 <knu9910@naver.com>
2025-06-24 13:43:14 +0900

    first init

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' },
+    ],
+  },
+  experimental: {
+    reactCompiler: true,
+  },
+  output: 'standalone',
+};
+
+export default nextConfig;
